4f4bdf38175799ba3845c4e287e88df89214c100
Jonas Sprenger <jonas.sprenger@gbtec.de>
2015-08-05 22:32:15 +0200

    some sugar for the example test data

--- examples/app.js
+++ examples/app.js
@@ -1,23 +1,27 @@
 (function () {
     angular.module('de.devjs.angular.pie.example', ['de.devjs.angular.pie'])
         .controller('appController', ['$scope', '$interval', function ($scope, $interval) {
-            $scope.pieData = [{
-                name: 'j.sprenger',
-                count: 56
-            }, {
-                name: 'stephan.koeninger',
-                count: 1000
-            },
+            $scope.pieData = [
                 {
+                    name: 'j.sprenger',
+                    count: 56
+                }, {
+                    name: 'some1',
+                    count: 10
+                }, {
+                    name: 'stekoe',
+                    count: 1000
+                }, {
                     name: 'end.gegner',
                     count: 1000
                 }];
 
             $interval(function () {
                 $scope.pieData = angular.copy($scope.pieData);
-                $scope.pieData[0].count = Math.random() * 128;
-                $scope.pieData[1].count = Math.random() * 55;
-                $scope.pieData[2].count = Math.random() * 255;
+                // just test data, do not mind those magic numbers!
+                $scope.pieData.map(function (i) {
+                    return i.count = Math.floor(Math.random() * (1000 - 1)) + 1;
+                });
             }, 1000);
         }])
 })();
